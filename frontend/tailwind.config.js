ca2204c7ec5f2cc4e9ec62b6b8ebe36dc5be25d0
Xiang Yu Tuang <xiangyutuang@gmail.com>
2024-02-26 00:04:57 -0500

    90% done for signup page css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'signup': "url('/src/assets/signup.png')",
+      }
+    },
+    colors: {
+      'blue':'#1183D0',
+      'dark-green':'#30503B',
+      'brown':'#994043',
+      'light-green':'#DBDFAC',
+      'beige':'#F4EBD9',
+    }
   },
   plugins: [],
 }
