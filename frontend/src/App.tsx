93c105a5e2d6f770606dc785bfd3d267df2d7a52
Xiang Yu Tuang <xiangyutuang@gmail.com>
2024-02-29 20:05:14 -0500

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,10 +17,3 @@ function App() {
 }
 
 export default App
-
-      {/* <div className="flex items-center gap-4 m-auto">
-        <h1 className="text-blue-500 font-bold m-auto text-lg">Welcome to LLW</h1>
-        <div>
-          <div className="bg-red-100 border-2 rounded-full text-xs font-bold text-red-500 border-red-500 px-2">v.1.0.0</div>
-        </div>
-      </div> */}
